4821daa36862ae90ef045d228a2a7ff8c137197b
ahrnjic3 <ahrnjic3@etf.unsa.ba>
2020-11-01 21:03:00 +0100

    Napravljena klasa Artikl sa konstruktorom, getterom i setterom za sve atribute

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private int cijena;
+    private int kod;
+
+    public Artikl(String naziv, int cijena, int kod){
+        this.naziv=naziv;
+        this.cijena=cijena;
+        this.kod=kod;
+    }
+
+    public int getCijena() {
+        return cijena;
+    }
+
+    public int getKod() {
+        return kod;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setCijena(int cijena) {
+        this.cijena = cijena;
+    }
+
+    public void setKod(int kod) {
+        this.kod = kod;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
 }
